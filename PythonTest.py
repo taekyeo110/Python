f087796958f5b569e044603dec535a4a1523340e
taekyeo110 <taekyeo110@outlook.com>
2020-07-18 16:23:46 +0900

    commit

--- PythonTest.py
+++ PythonTest.py
@@ -1,7 +1,7 @@
-a= [10,20,30]
-a.insert(len(a),600)
-print(a)
-print(a.pop(1))
-print(a)
-del a[1]
-print(a)
+a= [31,21,52,43,63]
+smallest = a[0]
+for i in a:
+    if i < smallest:
+        smallest=i
+
+print(smallest)
