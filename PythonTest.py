6e182bd8aa8fa91a07e8db40ed8ed99808de26fa
taekyeo110 <taekyeo110@outlook.com>
2020-07-20 10:43:48 +0900

    commit

--- PythonTest.py
+++ PythonTest.py
@@ -1,7 +1,33 @@
-a= [31,21,52,43,63]
-smallest = a[0]
-for i in a:
-    if i < smallest:
-        smallest=i
+'''
 
-print(smallest)
+keys = ['a','b','c','d']
+x=dict.fromkeys(keys)
+print(x)
+
+y=dict.fromkeys(keys,100)
+print(y)
+
+x = {'a':10,'b':20,'c':30,'d':40}
+for key in x.keys():
+    print(key,end=' ')
+
+keys = ['a','b','c','d']
+x={key:value for key, value in dict.fromkeys(keys).items()}
+print(x)
+
+fruits = {'strawberry','grape','orange','pineapple','cherry'}
+print('orange' in fruits)
+print('peach' in fruits)
+
+a=set('apple')
+print(a)
+b=set(range(5))
+print(b)
+c={}
+print(type(c))
+c=set()
+print(c)
+print(type(c))
+
+'''
+
