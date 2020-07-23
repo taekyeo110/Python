67241360c5c11d7a60072a366f6a57eb41e3ecde
taekyeo110 <taekyeo110@outlook.com>
2020-07-23 14:12:24 +0900

    commit

--- PythonTest.py
+++ PythonTest.py
@@ -642,6 +642,23 @@ try:
 except Exception as e:
     print('스크립트 파일에서 예외가 발생했습니다.', e)
 
+
+
+class NotThreeMultipleError(Exception):
+    def __init__(self):
+        super().__init__('3의 배수가 아닙니다. ')
+    
+def three_multiple():
+    try:
+        x = int(input('3의 배수를 입력하세요: '))
+        if x%3 != 0:
+            raise NotThreeMultipleError
+        print(x)
+    except Exception as e:
+        print('예외가 발생했습니다.',e)
+        
+three_multiple()
+
    
 
 '''
